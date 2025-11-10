df4d5396bde83420942b327203804d4fc2ed85be
Jona <62996543+JaskerX@users.noreply.github.com>
2025-11-11 00:12:00 +0100

    Change lobbies routes and add cors allowed origins (#6)
    * improve lobbies routes
    
    * add cors allowed origins

--- src/main/java/de/hexfieldsstudio/hexfieldsdominion/HexfieldsDominionApplication.java
+++ src/main/java/de/hexfieldsstudio/hexfieldsdominion/HexfieldsDominionApplication.java
@@ -2,16 +2,14 @@ package de.hexfieldsstudio.hexfieldsdominion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude = {
         org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration.class
 })
-@RestController
 public class HexfieldsDominionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HexfieldsDominionApplication.class, args);
-	}
+        SpringApplication.run(HexfieldsDominionApplication.class, args);
+    }
 
 }
