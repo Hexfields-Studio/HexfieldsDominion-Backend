f51fe03b87f71df76214e585e21d68674718c9fb
JaskerX <jaskerlp@gmx.de>
2026-05-26 00:06:27 +0200

    InvalidRadiusException extends BadRequestException now

--- src/main/java/de/hexfieldsstudio/hexfieldsdominion/lobby/error/InvalidRadiusException.java
+++ src/main/java/de/hexfieldsstudio/hexfieldsdominion/lobby/error/InvalidRadiusException.java
@@ -1,6 +1,8 @@
 package de.hexfieldsstudio.hexfieldsdominion.lobby.error;
 
-public class InvalidRadiusException extends RuntimeException {
+import de.hexfieldsstudio.hexfieldsdominion.error.BadRequestException;
+
+public class InvalidRadiusException extends BadRequestException {
     public InvalidRadiusException(int boardRadius) {
         super("error: Radius invalid for %s. It must be between 3 and 6.".formatted(boardRadius));
     }
