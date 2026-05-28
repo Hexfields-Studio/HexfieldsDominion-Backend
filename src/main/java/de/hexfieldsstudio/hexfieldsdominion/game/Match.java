c1280aba434d1abaec784b63f195b42be67599d5
Alexander Horst <118634038+A1exHorst@users.noreply.github.com>
2026-05-28 10:57:46 +0200

    Refactored Match class to avoid getting a god-class by implementing an additional class "FieldFactory" with "generateFields()" (#45)
    * Refactored Match class to avoid getting a god-class by implementing an additional class to generate Fields.
    
    * Forgot to delete the field "boardRadius" in the Match class
    
    * Remove boardRadius from Match constructor
    
    Removed boardRadius initialization from Match constructor.

--- src/main/java/de/hexfieldsstudio/hexfieldsdominion/game/Match.java
+++ src/main/java/de/hexfieldsstudio/hexfieldsdominion/game/Match.java
@@ -18,7 +18,6 @@ public class Match {
 
     @Getter
     private final List<PlayerRepresentation> players;
-    private final int boardRadius;
     @Getter
     private final List<Field> fields;
     private List<Structure> structures;
@@ -33,93 +32,16 @@ public class Match {
     public Match(UUID uuid, int boardRadius, Lobby lobby){
         this.uuid = uuid;
 
-        this.boardRadius = boardRadius;
-        this.fields = generateFields();
-
-        this.players = this.createPlayerRepresentationsForLobby(lobby);
-        this.playersTurnOrder = this.generatePlayersTurnOrder();
-    }
-
-    private int calculateTotalResourceFields(){
-        return 3 * boardRadius * (boardRadius - 1); //excluding center field
-    }
-
-    private List<Integer> generateNumberChips(){
-        List<Integer> numberChips = new ArrayList<>(List.of(2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12));
-        int totalFields = calculateTotalResourceFields();
-
-        SecureRandom random = new SecureRandom();
-        while (numberChips.size() < totalFields) {
-            int chip = random.nextInt(11) + 2; // 2–12
-            if (chip != 7) numberChips.add(chip);
-        }
-
-        Collections.shuffle(numberChips);
-        return numberChips;
-    }
-
-    private List<ResourceType> generateAvailableResourceTypes(Map<ResourceType, Float> ratios){
-        int totalFields = calculateTotalResourceFields();
-
-        List<ResourceType> types = new ArrayList<>(ratios.keySet());
-        Map<ResourceType, Integer> counts = new EnumMap<>(ResourceType.class);
-
-        int used = 0;
-
-        // initial floor allocation
-        for (var e : ratios.entrySet()) {
-            int count = (int) Math.floor(totalFields * e.getValue());
-            counts.put(e.getKey(), count);
-            used += count;
-        }
-
-        // distribute resources on remaining fields
-        int i = 0;
-        while (used < totalFields) {
-            ResourceType t = types.get(i % types.size());
-            counts.put(t, counts.get(t) + 1);
-            used++;
-            i++;
-        }
-
-        List<ResourceType> result = new ArrayList<>(totalFields);
-        for (var e : counts.entrySet()) {
-            for (int j = 0; j < e.getValue(); j++) {
-                result.add(e.getKey());
-            }
-        }
-
-        Collections.shuffle(result);
-        return result;
-    }
-
-    private List<Field> generateFields() {
-        List<Field> fields = new ArrayList<>();
-
-        List<ResourceType> availableResourceTypes = generateAvailableResourceTypes(Map.of(
+        Map<ResourceType, Float> ratios = Map.of(
                 ResourceType.WOOD, 0.3f,
                 ResourceType.BRICK, 0.2f,
                 ResourceType.WHEAT, 0.3f,
                 ResourceType.SHEEP, 0.2f
-        ));
-        List<Integer> numberChips = generateNumberChips();
+        );
+        this.fields = FieldFactory.generateFields(boardRadius, ratios);
 
-        // https://www.redblobgames.com/grids/hexagons/#coordinates-axial
-        for (int q = -boardRadius + 1; q <= boardRadius - 1; q++) {
-            int r1 = Math.max(-boardRadius + 1, -q - boardRadius + 1);
-            int r2 = Math.min(boardRadius - 1, -q + boardRadius - 1);
-            for (int r = r1; r <= r2; r++) {
-                if (q == 0 && r == 0) {
-                    fields.add(new Field(new AxialPosition(q, r), 0, ResourceType.DUNES));
-                    continue;
-                }
-                ResourceType type = availableResourceTypes.removeFirst();
-                int numberChip = numberChips.removeFirst();
-                fields.add(new Field(new AxialPosition(q, r), numberChip, type));
-            }
-        }
-
-        return fields;
+        this.players = this.createPlayerRepresentationsForLobby(lobby);
+        this.playersTurnOrder = this.generatePlayersTurnOrder();
     }
 
     private List<PlayerRepresentation> createPlayerRepresentationsForLobby(Lobby lobby) {
