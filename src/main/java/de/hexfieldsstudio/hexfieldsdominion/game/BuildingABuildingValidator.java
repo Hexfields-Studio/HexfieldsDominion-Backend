38ee603afc79637fe59351c86f3253bf986501e8
A1exHorst <alexhorst58@gmail.com>
2026-05-29 12:54:24 +0200

    WIP: Added computation of edges (for placing streets) to the BuildingABuildingValidator class. Both Corners (buildings) and streets (edges) can now be validated based on position

--- src/main/java/de/hexfieldsstudio/hexfieldsdominion/game/BuildingABuildingValidator.java
+++ src/main/java/de/hexfieldsstudio/hexfieldsdominion/game/BuildingABuildingValidator.java
@@ -17,18 +17,37 @@ public class BuildingABuildingValidator {
             List.of(AxialPosition.of(-1, 1), AxialPosition.of(-1, 0)),
             List.of(AxialPosition.of(-1, 0), AxialPosition.of(0, -1))
     ));
+    private final Set<List<AxialPosition>> edges;
+    private static final List<AxialPosition> edgeOffsetToAdjacentField = List.of(
+            AxialPosition.of(1, -1),
+            AxialPosition.of(1, 0),
+            AxialPosition.of(0, 1),
+            AxialPosition.of(-1, 1),
+            AxialPosition.of(-1, 0),
+            AxialPosition.of(0, -1)
+
+    );
 
     public BuildingABuildingValidator(List<Field> fields){
-        //Precompute Corners and Edges
-        corners = preComputeUniqueCornerPositions(fields);
+        corners = computeUniqueCorners(fields);
+        edges = computeUniqueEdges(fields);
     }
 
     public boolean validate(Match match, BuildActionDTO buildActionDTO) throws MissingAxialPositionsException{
         StructureType type = buildActionDTO.getStructureType();
         if (buildActionDTO.getPos().size() != type.getPosAmount()) throw new MissingAxialPositionsException(type, buildActionDTO.getPos().size());
         List<AxialPosition> sortedPos = getSortedPosition(buildActionDTO.getPos());
-        if (!isPositionValid(sortedPos)) return false;
+
+        boolean isValid = isPositionValid(sortedPos);
+        switch (type){
+            case TOWN -> isValid = corners.contains(sortedPos);
+            case STREET -> isValid = edges.contains(sortedPos);
+        }
+        System.out.printf("Type: %s; pos: %s; isValid: %s%n", type, sortedPos, isValid);
+        if (!isValid) return false;
+
         buildActionDTO.setPos(sortedPos);
+        Map<AxialPosition, Field> map = Field.getFieldsMap(match.getFields());
 
         return true;
     }
@@ -37,8 +56,9 @@ public class BuildingABuildingValidator {
         return corners.contains(pos);
     }
 
-    private Set<List<AxialPosition>> preComputeUniqueCornerPositions(List<Field> fields){
+    private Set<List<AxialPosition>> computeUniqueCorners(List<Field> fields){
         Set<List<AxialPosition>> cornerMap = new HashSet<>();
+
         for (Field field : fields){
             AxialPosition pos = field.pos();
 
@@ -56,6 +76,24 @@ public class BuildingABuildingValidator {
             }
         }
         return cornerMap;
+    }
+
+    private Set<List<AxialPosition>> computeUniqueEdges(List<Field> fields){
+        Set<List<AxialPosition>> edgeMap = new HashSet<>();
+
+        for (Field field : fields){
+            AxialPosition pos = field.pos();
+
+            for (int i = 0; i < 6; i++){
+                AxialPosition offset = edgeOffsetToAdjacentField.get(i);
+                List<AxialPosition> edgePos = new ArrayList<>(List.of(
+                        pos,
+                        AxialPosition.of(pos.q() + offset.q(), pos.r() + offset.r())
+                ));
+                edgeMap.add(getSortedPosition(edgePos));
+            }
+        }
+        return edgeMap;
     }
 
     private List<AxialPosition> getSortedPosition(List<AxialPosition> pos){
