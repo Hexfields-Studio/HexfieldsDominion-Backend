11d4fa2a22d4a9c59ac6d23f4540d7dc8cce537a
Alexander Horst <118634038+A1exHorst@users.noreply.github.com>
2026-06-02 01:10:40 +0200

    Revert "Player Color Generation using Hash Function (#52)" (#54)
    This reverts commit 60ca66b5526e4e4b1bb28c056bb5c90ead1cb120.

--- src/main/java/de/hexfieldsstudio/hexfieldsdominion/game/error/NotEnoughResourcesException.java
+++
@@ -1,9 +0,0 @@
-package de.hexfieldsstudio.hexfieldsdominion.game.error;
-
-import de.hexfieldsstudio.hexfieldsdominion.error.BadRequestException;
-
-public class NotEnoughResourcesException extends BadRequestException {
-    public NotEnoughResourcesException() {
-        super("You don't have enough resources to do that.");
-    }
-}
